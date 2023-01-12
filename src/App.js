a5b2d9df711f08bd5af5e2879d688f969af466b7
jtan-aps <jtan4@aceproject.space>
2023-01-12 09:25:10 -0600

    Initial Routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,7 @@
 import Todo from "./components/Todo";
 
 function App() {
-  return (
-    <div>
-      <h1>My Todos</h1>
-      <Todo text="Learn React" />
-      <Todo text="Master React" />
-      <Todo text="Use React" />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;

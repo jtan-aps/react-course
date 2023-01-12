8ab9a5e7e1cab0d2d931d908e0d3ad320c0aea2b
jtan-aps <jtan4@aceproject.space>
2023-01-12 10:18:55 -0600

    Modified App.js and Index.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
-import Todo from "./components/Todo";
+import { Route, Switch } from "react-router-dom";
+
+import AllMeetupsPage from "./pages/AllMeetups";
+import FavoritesPage from "./pages/Favorites";
+import NewMeetupPage from "./pages/NewMeetup";
+import MainNavigation from "./components/layout/MainNavigation";
 
 function App() {
-  return <div></div>;
+  // What to put in path v
+  // localhost:3000 is domain
+  // real site could be my-page.com/
+  return (
+    <div>
+      <MainNavigation />
+      <Switch>
+        <Route path="/" exact={true}>
+          <AllMeetupsPage />
+        </Route>
+        {/* <Route path="/" exact>
+          <AllMeetupsPage />
+        </Route> Also works because you can set it to a truthy value*/}
+        <Route path="/new-meetup">
+          <NewMeetupPage />
+        </Route>
+        <Route path="/favorites">
+          <FavoritesPage />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;

72e305286ad291a239292279376cbd1c9af95b64
jtan-aps <jtan4@aceproject.space>
2023-01-12 08:40:34 -0600

    Backdrop.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import Todo from "./components/Todo";
+import Modal from "./components/Modal";
+import Backdrop from "./components/Backdrop";
 
 function App() {
   return (
@@ -7,6 +9,8 @@ function App() {
       <Todo text="Learn React" />
       <Todo text="Master React" />
       <Todo text="Use React" />
+      <Modal />
+      <Backdrop />
     </div>
   );
 }

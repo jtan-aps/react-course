0dcd40aa2af0cf3e904436ac8729880018957e72
jtan-aps <jtan4@aceproject.space>
2023-01-18 09:15:12 -0600

    Completed Lesson 50

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,8 +1,12 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
 
 import classes from "./MainNavigation.module.css";
+import FavoritesContext from "../../store/favorites-context";
 
 function MainNavigation() {
+  const favoritesCtx = useContext(FavoritesContext);
+  
   return (
     <header className={classes.header}>
       <div className={classes.logo}>React Meetups</div>
@@ -16,6 +20,7 @@ function MainNavigation() {
           </li>
           <li>
             <Link to="/favorites">My Favorites</Link>
+            <span className={classes.badge}>{favoritesCtx.totalFavorites}</span>
           </li>
         </ul>
       </nav>
